6cd5fd1f3ffe4e1128474199f768aff15d2f6fc5
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-11-24 18:10:03 +0100

    iterate function
    first upload iterateInvestments function and some cleaning up

--- utils/activate.py
+++ utils/activate.py
@@ -1,33 +1,47 @@
-from pandas.core.indexes.base import Index
 from scrape_morningstar_selenium import getPrices, getPriceVar, getDirection, getYield
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, InvalidArgumentException
+from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
 
-def getData(url: str):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--incognito")
-    driver = webdriver.Chrome(options = chrome_options)
-    r = driver.get(url)
-    html = driver.page_source
+def getData(driver):
     time.sleep(10)
     price = getPrices(driver)
-    priceVar = getPriceVar(driver)
+    varVal, varPerc = getPriceVar(driver)
     direction = getDirection(driver)
     yieldPerYear = getYield(driver)
     driver.close()
-    return price, priceVar, direction, yieldPerYear
+    return price, varVal, varPerc, direction, yieldPerYear
 
-def itterateInvestments(excelFile: str):
+def iterateInvestments(excelFile: str):
     df = pd.read_excel(excelFile)
-    for item in Index:
-        # input on website
-        url = "" # new url
-        price, priceVar, direction, yieldPerYear = getData(url)
-        df.append(price, priceVar, direction, yieldPerYear)
+    for item in df["ISIN"]:
+        print(item)
+        url = "https://www.morningstar.com/search?query=" + item
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--incognito")
+        driver = webdriver.Chrome(options = chrome_options)
+        driver.get(url)
+
+        try:
+            driver.find_element(By.XPATH, "//section/div[2]/a[@data-v-5db0bc77]").click()
+            price, varVal, varPerc, direction, yieldPerYear = getData(driver)
+        except NoSuchElementException or InvalidArgumentException:
+            price, varVal, varPerc, direction, yieldPerYear = "", "", "", "Check Manually", ""
+            continue
+            
+        df["price"] = price
+        df["var (valuta)"] = varVal
+        df["var (%)"] = varPerc
+        df["yield per year"] = yieldPerYear
+    print(df)
 
 def singleInvestment(ISIN: str):
     url = str + ISIN
 
+excelFile = "C:/Users/pdewilde/Documents/Projects/AG2R/assets/data.xlsx"
+iterateInvestments(excelFile)
