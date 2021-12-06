657aa58b0629a8dcd8119202b285014dcedbdc05
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-12-06 12:26:59 +0100

    Create download_button.py

---
+++ utils/download_button.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from io import BytesIO
+import base64
+import streamlit as st
+
+
+def generate_excel_download_link(df):
+    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
+    date = datetime.today()
+    towrite = BytesIO()
+    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    towrite.seek(0)  # reset pointer
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download_{date}.xlsx">Download Excel File</a>'
+    return st.markdown(href, unsafe_allow_html=True)
